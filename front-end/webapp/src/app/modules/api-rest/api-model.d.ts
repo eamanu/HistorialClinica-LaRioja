ddf36e976f0aa95dd9a0e31777eb6968f050ce04
Julio <julioriveralaguna@gmail.com>
2020-10-09 13:49:06 -0300

    tg-1972 fix error 500 login backoffice - reset recaptcha

--- front-end/webapp/src/app/modules/api-rest/api-model.d.ts
+++ front-end/webapp/src/app/modules/api-rest/api-model.d.ts
@@ -891,6 +891,11 @@ export interface PublicInfoDto {
     flavor: string;
 }
 
+export interface RecaptchaPublicConfigDto {
+    enabled: boolean;
+    siteKey: string;
+}
+
 export interface ReducedPatientDto {
     patientTypeId: number;
     personalDataDto: BasicPersonalDataDto;
