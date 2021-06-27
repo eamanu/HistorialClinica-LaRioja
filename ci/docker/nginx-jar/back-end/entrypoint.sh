c1919f1904574adf95cb5adae15c0fac0c34139f
Emmanuel Arias <eamanu@yaerobi.com>
2021-06-27 19:07:43 -0300

    Experimental separate back and fron

--- ci/docker/nginx-jar/back-end/entrypoint.sh
+++ ci/docker/nginx-jar/back-end/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-nginx
+#nginx
 if [ ! -z "$GLOWROOT_AGENT_ID" ]; then
     echo "agent.id=$GLOWROOT_PROJECT::$GLOWROOT_AGENT_ID" > /glowroot/glowroot.properties
     echo "collector.address=http://10.125.125.79:8181" >> /glowroot/glowroot.properties
